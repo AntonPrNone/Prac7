0098c4b86c97b9a28e974691f40d1752721339af
Антон <61877084+AntonPrNone@users.noreply.github.com>
2024-02-02 14:56:44 +0300

    Добавьте файлы проекта.

---
+++ Prac7/ArrayOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac7
+{
+    public static class ArrayOperations
+    {
+        public static int[] arrayInt { get; set; } = Enumerable.Range(1, 10).ToArray();
+
+        public static int SumArray()  => arrayInt.Sum();
+
+        public static int MaxElArray() => arrayInt.Max();
+
+        public static void MultiplyByNumberArray(int multiplier) => arrayInt = arrayInt.Select(x => x * multiplier).ToArray();
+
+        public static double AverageElArray() => arrayInt.Average();
+
+    }
+}
---
+++ Prac7/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Prac7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            GetIntArrayFromUser();
+            Console.WriteLine(String.Join(" ", ArrayOperations.arrayInt));
+            Console.WriteLine(ArrayOperations.SumArray());
+            Console.WriteLine(ArrayOperations.MaxElArray());
+            //Console.WriteLine(ArrayOperations.MultiplyByNumberArray());
+            Console.WriteLine(ArrayOperations.AverageElArray());
+        }
+
+        static void GetIntArrayFromUser()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Введите целые числа через запятую:");
+            Console.ResetColor();
+
+            while (true)
+            {
+                try
+                {
+                    string? input = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        PrintErrorMessage("Введена пустая строка. Пожалуйста, введите целые числа.");
+                        continue;
+                    }
+
+                    string[] inputArray = input.Replace(" ", "").Split(',');
+
+                    int[] resultArray = new int[inputArray.Length];
+
+                    for (int i = 0; i < inputArray.Length; i++)
+                    {
+                        resultArray[i] = int.Parse(inputArray[i]);
+                    }
+
+                    ArrayOperations.arrayInt = resultArray;
+                    break;
+                }
+
+                catch (FormatException)
+                {
+                    PrintErrorMessage("Ошибка ввода. Пожалуйста, введите корректные целые числа.");
+                }
+
+                catch (Exception ex)
+                {
+                    PrintErrorMessage($"Произошла ошибка: {ex.Message}");
+                }
+            }
+        }
+
+        static void PrintErrorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
